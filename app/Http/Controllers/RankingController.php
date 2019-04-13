d7c580b209daf231b4a6a849c3719e4045799989
anderson <andnunes88@gmail.com>
2019-04-13 13:37:51 -0300

    pegar vendas por temporadas

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -7,6 +7,7 @@ use App\Venda;
 use Illuminate\Support\Facades\DB;
 use App\Categoria;
 use App\User;
+use App\Temporada;
 
 class RankingController extends Controller
 {
@@ -21,12 +22,15 @@ class RankingController extends Controller
     }
 
     public function pegarTodasVendas(){
+
+        $temporada = Temporada::where('status', 1)->first();
     	
         $vendas = DB::table('vendas')
             ->select('users.name', 'users.foto','unidades.uni_nome', 'categorias.cat_meta', DB::raw('SUM(vendas.ven_valor) as total_vendas'))
             ->join('users', 'users.id', 'vendas.ven_vendedor_id')
             ->join('unidades', 'unidades.id', 'vendas.ven_unidade_id')
             ->join('categorias', 'categorias.id', 'users.categoria_id')
+             ->Where('ven_temporada_id', $temporada->id)
             ->groupBy('ven_vendedor_id')
             ->orderBy('total_vendas', 'DESC')
             ->get();
@@ -37,6 +41,8 @@ class RankingController extends Controller
 
     public function pegarTodasVendasJson($id_categoria = null){
         
+        $temporada = Temporada::where('status', 1)->first();
+
         if($id_categoria != null){
 
             $vendas = DB::table('vendas')
@@ -45,6 +51,7 @@ class RankingController extends Controller
                 ->join('unidades', 'unidades.id', 'vendas.ven_unidade_id')
                 ->join('categorias', 'categorias.id', 'users.categoria_id')
                 ->Where('ven_categoria_id', $id_categoria)
+                ->Where('ven_temporada_id', $temporada->id)
                 ->groupBy('ven_vendedor_id')
                 ->orderBy('total_vendas', 'DESC')
                 ->get(); 
@@ -58,6 +65,7 @@ class RankingController extends Controller
                 ->join('users', 'users.id', 'vendas.ven_vendedor_id')
                 ->join('unidades', 'unidades.id', 'vendas.ven_unidade_id')
                 ->join('categorias', 'categorias.id', 'users.categoria_id')
+                ->Where('ven_temporada_id', $temporada->id)
                 ->groupBy('ven_vendedor_id')
                 ->orderBy('total_vendas', 'DESC')
                 ->get();
